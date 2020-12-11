504bfb4c51ab94eeb9ac9e74c7f42ef824e4a53d
seananth <seansafstrom@gmail.com>
2020-12-11 15:03:24 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { Heading, Page, List } from "@shopify/polaris";
+
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉</Heading>
+    <List>
+      <List.Item>
+        123
+      </List.Item>
+    </List>
+
+  </Page>
+);
+
+export default Index;
