f2de6e66c55e72b628094faeb4b071c697aa8060
seananth <seansafstrom@gmail.com>
2020-12-14 15:04:04 +0100

    added navigation

---
+++ pages/ActiveFlows.js
@@ -0,0 +1,11 @@
+import { Heading, Page, Navigation } from "@shopify/polaris";
+
+const ActiveFlows = () => (
+    <Page>
+      <Heading>Active Flows</Heading>
+
+    </Page>
+  );
+  
+  export default ActiveFlows;
+  
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,62 @@
-import { Heading, Page, List } from "@shopify/polaris";
-
+import { Heading, Page, Navigation } from "@shopify/polaris";
+import { Icon } from "@shopify/polaris";
+import {
+  CirclePlusMinor,
+  EditMajor,
+  AnalyticsMajor,
+  ActivitiesMajor,
+  StoreStatusMajor,
+} from "@shopify/polaris-icons";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-    <List>
-      <List.Item>
-        123
-      </List.Item>
-    </List>
+    <Heading>Flow Cart :P</Heading>
 
+    <Navigation location="/">
+      <Navigation.Section
+        items={[
+          {
+            url: "./ActiveFlows",
+            label: "Active Flows",
+            icon: ActivitiesMajor,
+          },
+          {
+            url: "/path/to/place",
+            label: "Analytics",
+            icon: AnalyticsMajor,
+          },
+          {
+            url: "/path/to/place",
+            label: "Usage",
+            icon: StoreStatusMajor,
+          },
+        ]}
+      />
+      <Navigation.Section
+        title="Build"
+        items={[
+          {
+            url: "/path/to/place",
+            label: "New Flow",
+            icon: CirclePlusMinor,
+          },
+          {
+            url: "/path/to/place",
+            label: "Edit Flow",
+            icon: EditMajor,
+          },
+        ]}
+        action={{
+          accessibilityLabel: "Add sales channel",
+          icon: CirclePlusMinor,
+          onClick: () => {},
+        }}
+      />
+    </Navigation>
   </Page>
 );
 
 export default Index;
+
+//icons
+//https://polaris-icons.shopify.com/?icon=StoreStatusMajor&q=stat
